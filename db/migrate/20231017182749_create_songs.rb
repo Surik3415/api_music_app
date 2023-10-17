97c225ef49dbe1ac72df9e5547efcc4ba9066bfc
surik3415 <suren_avakian@epam.com>
2023-10-17 21:30:51 +0300

    latest commit

--- db/migrate/20231017182749_create_songs.rb
+++ db/migrate/20231017182749_create_songs.rb
@@ -1,8 +1,7 @@
+# frozen_string_literal: true
+
 class CreateSongs < ActiveRecord::Migration[7.0]
   def change
-    create_table :songs do |t|
-
-      t.timestamps
-    end
+    create_table :songs, &:timestamps
   end
 end
