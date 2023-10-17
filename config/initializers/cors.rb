8ac377315bed2a49f2486123eb7d062495217d7e
surik3415 <suren_avakian@epam.com>
2023-10-17 05:09:46 +0300

    latest commit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('BASE_URL')
+    origins 'http://127.0.0.1:3000'
 
     resource '*', headers: :any, methods: %i[get post put patch delete options head]
   end
